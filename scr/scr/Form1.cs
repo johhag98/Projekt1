27eead5a94c62843f100869369e4f557c7c71a39
Fisksword <te14.john.hjalsten@nykopingsgymnasium.com>
2015-11-20 14:36:05 +0100

    Med kommentarer

--- scr/scr/Form1.cs
+++ scr/scr/Form1.cs
@@ -20,10 +20,11 @@ namespace scr
 
         private void btnGo_Click(object sender, EventArgs e)
         {
+            //För att läsa in alla tal, kolla om talet är en int, om inte blir bool false.
             string text1 = tbxTal1.Text;
             int tal1;
             bool test1 = int.TryParse(text1, out tal1);
-
+            
             string text2 = tbxTal2.Text;
             int tal2;
             bool test2 = int.TryParse(text2, out tal2);
@@ -40,14 +41,18 @@ namespace scr
             int tal5;
             bool test5 = int.TryParse(text5, out tal5);
 
-            if (test1 == false || test2 == false || test3 == false || test4 == false || test5 == false)
+            //Kolla om någon bool är false.
+            if (test1 == false || test2 == false || test3 == false || test4 == false || test5 == false)             
             {
+                //Om någon bool är false öppnas en messagebox.
                 MessageBox.Show("Du måste skriva in ett heltal!", "Medelande",MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
+            //För att Lägga alla tal i en array och välja ut det största talet.
             int[] array1 = { tal1, tal2, tal3, tal4, tal5 };
             int max = array1.Max();
 
+            //För att skriva ut det största talet.
             lblAns.Text = "Svar: " + max; 
             
 
